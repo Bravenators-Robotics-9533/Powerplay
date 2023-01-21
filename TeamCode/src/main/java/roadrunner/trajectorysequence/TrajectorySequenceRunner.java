2fe8e7244758f9e84ce6d91f7b6e6b3286afd72a
Nick Fanelli <nickfanelli5@gmail.com>
2023-01-20 20:14:23 -0500

    fix: roadrunner package names

--- TeamCode/src/main/java/roadrunner/trajectorysequence/TrajectorySequenceRunner.java
+++ TeamCode/src/main/java/roadrunner/trajectorysequence/TrajectorySequenceRunner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence;
+package roadrunner.trajectorysequence;
 
 import androidx.annotation.Nullable;
 
@@ -16,11 +16,11 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryMarker;
 import com.acmerobotics.roadrunner.util.NanoClock;
 
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.SequenceSegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TrajectorySegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TurnSegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.WaitSegment;
-import org.firstinspires.ftc.teamcode.util.DashboardUtil;
+import roadrunner.trajectorysequence.sequencesegment.SequenceSegment;
+import roadrunner.trajectorysequence.sequencesegment.TrajectorySegment;
+import roadrunner.trajectorysequence.sequencesegment.TurnSegment;
+import roadrunner.trajectorysequence.sequencesegment.WaitSegment;
+import roadrunner.util.DashboardUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
