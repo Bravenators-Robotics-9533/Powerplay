6d03ee1139336a2f7b513d829c6cd82dba2492dc
Nick Fanelli <nickfanelli5@gmail.com>
2023-02-12 09:42:55 -0500

    chore: test trajectories in autonomous

--- TeamCode/src/main/java/com/bravenatorsrobtoics/autonomous/RedAutonomousPath.java
+++ TeamCode/src/main/java/com/bravenatorsrobtoics/autonomous/RedAutonomousPath.java
@@ -1,21 +1,40 @@
 package com.bravenatorsrobtoics.autonomous;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RedAutonomousPath extends AbstractAutonomousPath {
 
-    private Trajectory lineupInitialConeTrajectory;
+    private final Trajectory lineupInitialConeTrajectory;
 
     public RedAutonomousPath(HardwareMap hardwareMap) {
         super(hardwareMap);
 
-//        lineupInitialConeTrajectory = drive.trajectoryBuilder(new Pose2d())
+        // Trajectory Definitions
+
+        lineupInitialConeTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .forward(54)
+                .strafeRight(12)
+                .build();
     }
 
     @Override
     public void Run() {
+
+        // Engage Cone Cam
+
+        // Synchronized Lift to Cruise Position
+
+        // Async Lift to Stage 3
+
+        // Follow Trajectory to Initial Cone Drop-off
+        drive.followTrajectory(lineupInitialConeTrajectory);
+
+        // Disengage Cone Cam
+
+
 
     }
 
